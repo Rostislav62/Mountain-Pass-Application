2f33b30cebabc5d68a7fcf03f48ba0232b8a32cf
Rostislav Smiglic <33193719+Rostislav62@users.noreply.github.com>
2025-02-17 09:34:59 +0000

    Исправлена конфигурация базы данных для Railway
    "Исправлена конфигурация базы данных для Railway"
     "Добавлен dj-database-url для работы с Railway"
    Внесены изменения в requirements.txt
    "Обновлены зависимости проекта"
    "Используем DATABASE_URL из Railway"

--- main/config/settings.py
+++ main/config/settings.py
@@ -1,7 +1,10 @@
 #  /Mountain Pass Application/config/settings.py
 
 from pathlib import Path
-import logging
+import os
+from dotenv import load_dotenv
+
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -17,8 +20,9 @@ SECRET_KEY = 'django-insecure-err^oskgs2y1-p8x4v6c418r22dk9n9p#d*%eyhj_$i4*o75m(
 DEBUG = True
 DEBUG_PROPAGATE_EXCEPTIONS = True  # Выводим полные ошибки в консоль
 
-ALLOWED_HOSTS = []
+import os
 
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
 
 # Application definition
 
@@ -62,21 +66,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mountain_pass_db',
-        'USER': 'postgres',
-        'PASSWORD': '1',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
 
 
 
@@ -124,25 +113,67 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 TEST_RUNNER = "django.test.runner.DiscoverRunner"
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'debug.log',  # Логи будут записываться в этот файл
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file', 'console'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'file': {
+#             'level': 'DEBUG',
+#             'class': 'logging.FileHandler',
+#             'filename': 'debug.log',  # Логи будут записываться в этот файл
+#         },
+#         'console': {
+#             'level': 'DEBUG',
+#             'class': 'logging.StreamHandler',
+#         },
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['file', 'console'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         },
+#     },
+# }
+
+MEDIA_URL = '/media/'  # URL для доступа к медиа-файлам
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Директория для сохранения медиа
+
+
+load_dotenv()  # Загружаем переменные из .env
+
+GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
+OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
+STRAVA_API_KEY = os.getenv("STRAVA_API_KEY")
+YANDEX_MAPS_API_KEY = os.getenv("YANDEX_MAPS_API_KEY")
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mountain_pass_db',
+#         'USER': 'postgres',
+#         'PASSWORD': '1',
+#         'HOST': 'localhost',
+#         'PORT':5432,
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'HOST': os.getenv('FSTR_DB_HOST'),
+#         'PORT': os.getenv('FSTR_DB_PORT'),
+#         'NAME': os.getenv('FSTR_DB_NAME'),
+#         'USER': os.getenv('FSTR_LOGIN'),
+#         'PASSWORD': os.getenv('FSTR_PASS'),
+#     }
+# }
+
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"), conn_max_age=600)
 }
+
