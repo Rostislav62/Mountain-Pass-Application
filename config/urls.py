d8eb86c0ebb12fc18cc7f347d4619b2316cacd95
Rostislav Smiglic <33193719+Rostislav62@users.noreply.github.com>
2025-02-18 14:04:15 +0000

    Добавлена поддержка Swagger и Redoc
    Добавлена поддержка Swagger и Redoc

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,13 @@
 #  /Mountain Pass Application/config/urls.py
 
 from django.http import JsonResponse
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 # Функция заглушка для главной страницы
 def index(request):
@@ -21,3 +23,22 @@ urlpatterns = [
 # Добавляем маршруты для обработки медиа-файлов **только в режиме DEBUG**
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+    schema_view = get_schema_view(
+        openapi.Info(
+            title="Mountain Pass API",
+            default_version="v1",
+            description="Документация API для проекта Mountain Pass Application",
+        ),
+        public=True,
+        permission_classes=[permissions.AllowAny],
+    )
+
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('api/', include('main.urls')),  # Основные API-маршруты
+        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    ]
