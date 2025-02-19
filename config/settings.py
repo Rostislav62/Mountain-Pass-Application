7e3401851cac5472167dbc1b1a1e4210e8d01db2
Rostislav Smiglic <33193719+Rostislav62@users.noreply.github.com>
2025-02-19 14:48:09 +0000

    Fix: Swagger JSON и порядок INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -39,9 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'rest_framework',  # Подключаем DRF
     'main',
-    'drf_yasg',
+
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -57,6 +57,7 @@ schema_view = get_schema_view(
 urlpatterns += [
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^swagger.json$', schema_view.without_ui(cache_timeout=0), name='schema-json'),  # Добавляем Swagger JSON
 ]
 
 
@@ -68,8 +69,4 @@ def list_urls(request):
 
 urlpatterns += [
     path('debug/urls/', list_urls),  # Временный эндпоинт для просмотра маршрутов
-]
-
-urlpatterns += [
-    re_path(r'^swagger.json$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
