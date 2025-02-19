c2d6ce4f698c8c169622d42f8733481141c753b8
Rostislav Smiglic <33193719+Rostislav62@users.noreply.github.com>
2025-02-19 12:04:41 +0000

    Remove Swagger authentication in production

--- config/urls.py
+++ config/urls.py
@@ -52,7 +52,7 @@ schema_view = get_schema_view(
     permission_classes=[permissions.AllowAny],
 )
 
-urlpatterns = [
+urlpatterns += [
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
