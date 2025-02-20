dd110aeb3a1b357600e1e553023f88cd83ddc7ea
Rostislav Smiglic <33193719+Rostislav62@users.noreply.github.com>
2025-02-20 12:59:01 +0000

    BASE_DIR
    BASE_DIR / "staticfiles/drf-yasg/swagger-ui-dist",  # Добавляем путь к Swagger UI

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    BASE_DIR / "staticfiles/swagger-ui-dist",  # Добавляем путь к Swagger UI
+    BASE_DIR / "staticfiles/drf-yasg/swagger-ui-dist",  # Добавляем путь к Swagger UI
 ]
 
 
