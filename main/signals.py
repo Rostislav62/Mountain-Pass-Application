30ab44b95fbe31470e986b1d557894005a7507e1
Rostislav Smiglic <33193719+Rostislav62@users.noreply.github.com>
2025-02-25 09:39:08 +0000

    Update signals.py

--- main/signals.py
+++ main/signals.py
@@ -1,16 +1,3 @@
+#  /Mountain Pass Application/main/signals.py
 
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.contrib.auth.models import User
-from main.models import UserProfile
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    """Автоматически создаёт профиль пользователя при создании `User`"""
-    if created:
-        UserProfile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    """Сохраняем профиль при изменении `User`"""
-    instance.profile.save()
