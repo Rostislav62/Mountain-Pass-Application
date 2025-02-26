43f2c49723f34763157942d6af20f8a6d0d6acdd
Rostislav Smiglic <33193719+Rostislav62@users.noreply.github.com>
2025-02-26 11:01:18 +0000

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -240,7 +240,7 @@ SWAGGER_SETTINGS = {
 }
 
 
-Создаём папку для логов, если её нет
+# Создаём папку для логов, если её нет
 LOGS_DIR = os.path.join(BASE_DIR, 'logs')
 if not os.path.exists(LOGS_DIR):
     os.makedirs(LOGS_DIR)
